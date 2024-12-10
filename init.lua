193c83d1dcd2498891dc1f87d329ef632aac12ea
geometriccross <geometriccross@gmail.com>
2024-12-10 22:08:00 +0900

    move setting parts into settings.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
+-- load my own lua files
 require 'custom.keymaps'
 require 'custom.autocmd'
+require 'custom.settings'
+
 -- require 'kickstart.plugins.debug'
 require 'kickstart.plugins.indent_line'
 -- require 'kickstart.plugins.lint'
